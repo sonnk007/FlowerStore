701f22aba8d2db1629c17d498bfa7a389ce60f92
ksonn <sonnkhe151341@fpt.edu.vn>
2024-05-06 22:23:16 +0700

    started

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddEmp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditEmp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelEmp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadEmpData_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchEmp_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
